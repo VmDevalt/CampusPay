ef0809ad98fb3599a8ab370b702fd8c79537255c
Victor <victor.montes@montreal.com.br>
2025-11-25 16:37:26 -0300

    Método cadastrar Usuário na base

--- src/main/java/com/churrasqueiro/data/UsuarioDAO.java
+++ src/main/java/com/churrasqueiro/data/UsuarioDAO.java
@@ -8,6 +8,7 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.Optional;
 
 public class UsuarioDAO {
@@ -37,5 +38,33 @@ public class UsuarioDAO {
             throw new DatabaseException("Falha ao consultar usuário no banco de dados.");
         }
         return Optional.empty();
+    }
+    
+    public Usuario cadastrarUsuario(Usuario usuario) throws DatabaseException {
+    	String sql = "INSERT INTO usuario (login, senha, tipo, email) VALUES (?, ?, ?, ?)";
+		
+		try (Connection conn = DatabaseConnection.getConnection();
+			 PreparedStatement ps = conn.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS)) {
+			
+			ps.setString(1, usuario.getLogin());
+			ps.setString(2, usuario.getSenhaHash());
+			ps.setString(3, usuario.getTipo());
+			ps.setString(4, usuario.getEmail());
+			
+			int linhasAfetadas = ps.executeUpdate();
+			
+			if (linhasAfetadas > 0) {
+                try (ResultSet rs = ps.getGeneratedKeys()) {
+                    if (rs.next()) {
+                        usuario.setId(rs.getInt(1));
+                    }
+                }
+                return usuario;
+            }
+		} catch (SQLException e) {
+			System.err.println("Erro ao cadastrar usuário: " + e.getMessage());
+			throw new DatabaseException("Falha ao cadastrar usuário no banco de dados.");
+		}
+		throw new DatabaseException("Falha desconhecida ao inserir usuário.");
     }
 }
