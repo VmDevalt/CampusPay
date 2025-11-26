3bc9b5d563e14d3f36275fdaa3f08d0a8381ddaa
Victor <victor.montes@montreal.com.br>
2025-11-26 10:27:50 -0300

    Detalhes tela de Menu Principal

--- src/main/java/com/churrasqueiro/ui/MenuPrincipal.java
+++ src/main/java/com/churrasqueiro/ui/MenuPrincipal.java
@@ -5,6 +5,8 @@ import com.churrasqueiro.ui.EstilizacaoRedonda;
 import java.awt.Color;
 import java.awt.EventQueue;
 import java.awt.Font;
+import java.awt.Image;
+import java.awt.Toolkit;
 
 import javax.swing.ImageIcon;
 import javax.swing.JFrame;
@@ -45,11 +47,22 @@ public class MenuPrincipal extends JFrame {
 		setBounds(100, 100, 450, 300);
         setSize(LARGURA, ALTURA);
         setResizable(false);
+        setTitle("Menu Principal - Churrasqueiro");
 		contentPane = new JPanel();
 		contentPane.setBackground(new Color(227,202,187));
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
+		
+		java.net.URL url = getClass().getResource("/assets/imagens/iconeJanela.png");
+        if (url != null) {
+            try {
+                java.awt.Image icon = javax.imageio.ImageIO.read(url);
+                setIconImage(icon);
+            } catch (java.io.IOException e) {
+                System.err.println("Falha de I/O ao ler a imagem: " + e.getMessage());
+            }
+        }
 		
 		final EstilizacaoRedonda.BotaoRedondo botaoPedidos = new EstilizacaoRedonda.BotaoRedondo("PEDIDOS",corPaletaVermelho,corPaletaVermelhoInteracao,corPaletaVermelhoPressionado,35);
 		botaoPedidos.setText("Pedidos");
@@ -81,5 +94,31 @@ public class MenuPrincipal extends JFrame {
 		botaoSair.setBounds(1133, 39, 83, 38);
 		botaoPedidos.setFont(new Font("SansSerif", Font.BOLD, 45));
         contentPane.add(botaoSair);
+        
+        JLabel labelEscolhaUmaOpcao = new JLabel("Escolha uma opção");
+        labelEscolhaUmaOpcao.setFont(new Font("SansSerif", Font.BOLD, 40));
+        labelEscolhaUmaOpcao.setBounds(437, 178, 523, 58);
+        contentPane.add(labelEscolhaUmaOpcao);
+        
+        
+        JLabel labelLogo = new JLabel("");
+        labelLogo.setBounds(576, 22, 265, 165);
+        contentPane.add(labelLogo);
+        
+        ImageIcon iconLogo = null;
+        int larguraLogo = 80;
+        int alturaLogo = 80;
+        java.net.URL urlLogo = getClass().getResource("/assets/imagens/logo.png");
+        if (urlLogo != null) {
+            Image originalImage = Toolkit.getDefaultToolkit().getImage(urlLogo);
+            Image resizedImage = originalImage.getScaledInstance(
+                    larguraLogo,
+                    alturaLogo,
+                    Image.SCALE_SMOOTH
+            );
+            iconLogo = new ImageIcon(resizedImage);
+            labelLogo.setIcon(iconLogo);
+        }
+
 	}
 }
