198f903c5a05a91f2cd863b0fc7b34d9a12a24b1
VictorS <victor.user.scds@gmail.com>
2025-11-26 11:26:02 -0300

    Atualização da TelaCadastro
    Ajustes feitos para poder receber bordas arredondadas e adição de um JComboBox com tipos de usuário.

--- src/main/java/com/churrasqueiro/ui/TelaCadastro.java
+++ src/main/java/com/churrasqueiro/ui/TelaCadastro.java
@@ -10,23 +10,27 @@ import javax.swing.border.LineBorder;
 import java.awt.Color;
 import javax.swing.JTextField;
 import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 import javax.swing.JTextPane;
 import javax.swing.JLabel;
 import javax.swing.SwingConstants;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JPasswordField;
+import javax.swing.JComboBox;
 
 public class TelaCadastro extends JFrame {
 
 	private static final long serialVersionUID = 1L;
 	private JPanel panelVermelho;
-	private JTextField textFieldNome;
-	private JTextField txtDigiteSeuCpf;
-	private JTextField txtDigiteSeuTelefone;
-	private JTextField txtDigiteSeuEmail;
-	private JPasswordField txtDigiteSenha;
-	private JPasswordField txtConfirmeSenha;
+	private static final int LARGURA = 1280;
+	private static final int ALTURA = 720;
+	private EstilizacaoRedonda.CaixaTextoRedonda campoLogin;
+	private EstilizacaoRedonda.CaixaSenhaRedonda campoSenha;
+	private EstilizacaoRedonda.CaixaTextoRedonda campoNome;
+	private JButton botaoCriarConta;
 
 	
 	public static void main(String[] args) {
@@ -44,137 +48,114 @@ public class TelaCadastro extends JFrame {
 
 	
 	public TelaCadastro() {
+		
+		 Color corPaletaVermelho = new Color(179,13,36);
+	     Color corPaletaBege = new Color(227,202,187);
+	     Color corPaletaVermelhoInteracao = new Color(200,50,50);
+	     Color corPaletaVermelhoPressionado = new Color(150,0,0);
+	     Color corPaletaPreto = new Color(0,0,0);
+	     Color corPaletaPretoInteração = new Color(35,35,35);
+	     Color corPaletaCinza = new Color(140,127,127);
+		
 		setTitle("Cadastro - Churrasqueiro");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 1280, 720);
-		panelVermelho = new JPanel();
-		panelVermelho.setForeground(new Color(230, 230, 250));
-		panelVermelho.setBackground(new Color(179, 13, 36));
+		setSize(LARGURA, ALTURA);
+	    setResizable(false);
+	    setLocationRelativeTo(null);
+	        
+	    panelVermelho = new JPanel();
+        panelVermelho.setBackground(new Color(179, 13, 36));
 		panelVermelho.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(panelVermelho);
 		panelVermelho.setLayout(null);
-		
-		JPanel panelBranco = new JPanel();
-		panelBranco.setToolTipText("Digite seu nome...");
-		panelBranco.setBackground(new Color(227, 202, 187));
-		panelBranco.setBounds(97, 90, 1098, 505);
+
+        final EstilizacaoRedonda.PainelRedondo panelBranco = new EstilizacaoRedonda.PainelRedondo(null,60,4,corPaletaBege,null);
+        panelBranco.setFocusable(true);
+        panelBranco.requestFocusInWindow();
+		panelBranco.setBounds(90, 85, 1098, 505);
 		panelVermelho.add(panelBranco);
 		panelBranco.setLayout(null);
 		
-		JLabel textCadastrUsuario = new JLabel("Cadastro de Usúario");
-		textCadastrUsuario.setBounds(384, 12, 378, 44);
-		textCadastrUsuario.setFont(new Font("Calibri", Font.PLAIN, 40));
-		panelBranco.add(textCadastrUsuario);
+		this.campoNome = new EstilizacaoRedonda.CaixaTextoRedonda("Digite seu nome completo...",corPaletaVermelho,corPaletaBege,corPaletaCinza,2,35);
+		campoNome.setFont(new Font("Calibri", Font.PLAIN, 14));
+		campoNome.setToolTipText("Digite seu texto");
+		campoNome.setBounds(250, 189, 597, 38);
+		panelBranco.add(campoNome);
+		campoNome.setColumns(10);
 		
-		JLabel lblLogo = new JLabel("");
-		lblLogo.setBounds(993, 399, 82, 80);
-		lblLogo.setHorizontalAlignment(SwingConstants.CENTER);
-		lblLogo.setBackground(new Color(0, 0, 0));
-		lblLogo.setIcon(new ImageIcon("/home/victorscds/Documentos/GitHub/CampusPay/src/main/resources/assets/imagens/logoPequena.png"));
-		panelBranco.add(lblLogo);
+		this.campoLogin = new EstilizacaoRedonda.CaixaTextoRedonda("Digite seu email...",corPaletaVermelho,corPaletaBege,corPaletaCinza,2,35);
+		campoLogin.setFont(new Font("Calibri", Font.PLAIN, 14));
+		campoLogin.setToolTipText("Digite seu texto");
+		campoLogin.setBounds(250, 280, 597, 38);
+		panelBranco.add(campoLogin);
+		campoLogin.setColumns(10);
 		
-		JLabel lblNomeCompleto = new JLabel("Nome Completo");
-		lblNomeCompleto.setBounds(459, 68, 197, 44);
-		lblNomeCompleto.setHorizontalAlignment(SwingConstants.CENTER);
-		lblNomeCompleto.setFont(new Font("Calibri", Font.PLAIN, 18));
-		panelBranco.add(lblNomeCompleto);
+		this.campoSenha = new EstilizacaoRedonda.CaixaSenhaRedonda("Digite sua senha...",corPaletaVermelho,corPaletaBege,corPaletaCinza,2,35);
+		campoSenha.setToolTipText("Digite seu texto");
+		campoSenha.setFont(new Font("Calibri", Font.PLAIN, 14));
+		campoSenha.setColumns(10);
+		campoSenha.setBounds(250, 367, 597, 38);
+		panelBranco.add(campoSenha);
 		
-		textFieldNome = new JTextField();
-		textFieldNome.setBounds(333, 114, 454, 31);
-		textFieldNome.setToolTipText("Digite seu texto");
-		textFieldNome.setText("Digite seu nome completo...");
-		textFieldNome.setFont(new Font("Calibri", Font.PLAIN, 18));
-		textFieldNome.setForeground(Color.BLACK);
-		textFieldNome.setBackground(new Color(227, 202, 187));
-		panelBranco.add(textFieldNome);
-		textFieldNome.setColumns(10);
-		textFieldNome.setBorder(new LineBorder(new Color(191, 63, 63), 2));
+		JLabel labelCadastro = new JLabel("Cadastro de Usuário");
+		labelCadastro.setHorizontalAlignment(SwingConstants.CENTER);
+	    labelCadastro.setForeground(corPaletaPreto);
+	    labelCadastro.setFont(new Font("Calibri", Font.PLAIN, 28));
+	    labelCadastro.setBounds(349, 0, 345, 32);
+	    panelBranco.add(labelCadastro);
 		
-		JLabel lblCPF = new JLabel("CPF");
-		lblCPF.setBounds(284, 167, 197, 44);
-		lblCPF.setHorizontalAlignment(SwingConstants.CENTER);
-		lblCPF.setFont(new Font("Calibri", Font.PLAIN, 18));
-		panelBranco.add(lblCPF);
+		JLabel labelLogin = new JLabel("Login");
+	    labelLogin.setForeground(corPaletaPreto);
+	    labelLogin.setFont(new Font("Calibri", Font.PLAIN, 17));
+	    labelLogin.setBounds(505, 239, 58, 32);
+	    panelBranco.add(labelLogin);
+			
+	    JLabel labelSenha = new JLabel("Senha");
+	    labelSenha.setForeground(corPaletaPreto);
+	    labelSenha.setFont(new Font("Calibri", Font.PLAIN, 17));
+	    labelSenha.setBounds(505, 330, 71, 32);
+	    panelBranco.add(labelSenha);
 		
-		JLabel lblTelefone = new JLabel("Telefone");
-		lblTelefone.setBounds(627, 167, 197, 44);
-		lblTelefone.setHorizontalAlignment(SwingConstants.CENTER);
-		lblTelefone.setFont(new Font("Calibri", Font.PLAIN, 18));
-		panelBranco.add(lblTelefone);
+	    JLabel labelNomeCompleto = new JLabel("Nome Completo");
+	    labelNomeCompleto.setForeground(corPaletaPreto);
+		labelNomeCompleto.setFont(new Font("Calibri", Font.PLAIN, 17));
+		labelNomeCompleto.setBounds(465, 147, 129, 32);
+		panelBranco.add(labelNomeCompleto);
 		
-		txtDigiteSeuCpf = new JTextField();
-		txtDigiteSeuCpf.setBounds(223, 211, 312, 31);
-		txtDigiteSeuCpf.setToolTipText("Digite seu texto");
-		txtDigiteSeuCpf.setText("Digite seu CPF...");
-		txtDigiteSeuCpf.setForeground(Color.BLACK);
-		txtDigiteSeuCpf.setFont(new Font("Calibri", Font.PLAIN, 18));
-		txtDigiteSeuCpf.setColumns(10);
-		txtDigiteSeuCpf.setBorder(new LineBorder(new Color(191, 63, 63), 2));
-		txtDigiteSeuCpf.setBackground(new Color(227, 202, 187));
-		panelBranco.add(txtDigiteSeuCpf);
+		String[] tipoUsuarios = {"Administrador", "Funcionário"};
+		JComboBox cBoxTipoUsuario = new JComboBox(tipoUsuarios);
+		cBoxTipoUsuario.setBorder(new LineBorder(new Color(179, 13, 36), 1));
+		cBoxTipoUsuario.setForeground(corPaletaPreto);
+		cBoxTipoUsuario.setBackground(corPaletaBege);
+		cBoxTipoUsuario.setFont(new Font("Calibri", Font.PLAIN, 17));
+		cBoxTipoUsuario.setMaximumRowCount(2);
+		cBoxTipoUsuario.setBounds(424, 102, 202, 23);
+		panelBranco.add(cBoxTipoUsuario);
 		
-		txtDigiteSeuTelefone = new JTextField();
-		txtDigiteSeuTelefone.setBounds(576, 211, 312, 31);
-		txtDigiteSeuTelefone.setToolTipText("Digite seu texto");
-		txtDigiteSeuTelefone.setText("Digite seu Telefone...");
-		txtDigiteSeuTelefone.setForeground(Color.BLACK);
-		txtDigiteSeuTelefone.setFont(new Font("Calibri", Font.PLAIN, 18));
-		txtDigiteSeuTelefone.setColumns(10);
-		txtDigiteSeuTelefone.setBorder(new LineBorder(new Color(191, 63, 63), 2));
-		txtDigiteSeuTelefone.setBackground(new Color(227, 202, 187));
-		panelBranco.add(txtDigiteSeuTelefone);
+		JLabel labelTipoUsuario = new JLabel("Tipo de Usuário");
+		labelTipoUsuario.setForeground(corPaletaPreto);
+		labelTipoUsuario.setFont(new Font("Calibri", Font.PLAIN, 17));
+		labelTipoUsuario.setBounds(465, 58, 129, 32);
+		panelBranco.add(labelTipoUsuario);
 		
-		JLabel lblEmail = new JLabel("Email");
-		lblEmail.setBounds(459, 255, 197, 44);
-		lblEmail.setHorizontalAlignment(SwingConstants.CENTER);
-		lblEmail.setFont(new Font("Calibri", Font.PLAIN, 18));
-		panelBranco.add(lblEmail);
+		this.botaoCriarConta = new EstilizacaoRedonda.BotaoRedondo("Criar Conta",corPaletaVermelho,corPaletaVermelhoInteracao,corPaletaVermelhoPressionado,35);
+	//	botaoLogar.addActionListener(new ActionListener() {
+	//		public void actionPerformed(ActionEvent e) {
+	//			cadastrar();
+	//		}
+	//	});
+		botaoCriarConta.setForeground(new Color(255, 255, 255));
+		botaoCriarConta.setBackground(new Color(179, 13, 36));
+		botaoCriarConta.setBounds(399, 433, 268, 38);
+        botaoCriarConta.setFont(new Font("Calibri", Font.PLAIN, 17));
+        panelBranco.add(botaoCriarConta);
+        
+        JLabel labelLogo = new JLabel("");
+        labelLogo.setIcon(new ImageIcon("src/main/resources/assets/imagens/logoPequena.png"));
+        labelLogo.setBounds(996, 394, 90, 99);
+        panelBranco.add(labelLogo);
+        java.net.URL urlLogo = getClass().getResource("src/main/resources/assets/imagens/logoPequena.png");
 		
-		txtDigiteSeuEmail = new JTextField();
-		txtDigiteSeuEmail.setBounds(333, 293, 454, 31);
-		txtDigiteSeuEmail.setToolTipText("Digite seu texto");
-		txtDigiteSeuEmail.setText("Digite seu Email...");
-		txtDigiteSeuEmail.setForeground(Color.BLACK);
-		txtDigiteSeuEmail.setFont(new Font("Calibri", Font.PLAIN, 18));
-		txtDigiteSeuEmail.setColumns(10);
-		txtDigiteSeuEmail.setBorder(new LineBorder(new Color(191, 63, 63), 2));
-		txtDigiteSeuEmail.setBackground(new Color(227, 202, 187));
-		panelBranco.add(txtDigiteSeuEmail);
-		
-		JLabel lblSenha = new JLabel("Senha");
-		lblSenha.setBounds(284, 355, 197, 44);
-		lblSenha.setHorizontalAlignment(SwingConstants.CENTER);
-		lblSenha.setFont(new Font("Calibri", Font.PLAIN, 18));
-		panelBranco.add(lblSenha);
-		
-		JLabel lblComfirmarSenha = new JLabel("Confirmar Senha");
-		lblComfirmarSenha.setBounds(627, 355, 197, 44);
-		lblComfirmarSenha.setHorizontalAlignment(SwingConstants.CENTER);
-		lblComfirmarSenha.setFont(new Font("Calibri", Font.PLAIN, 18));
-		panelBranco.add(lblComfirmarSenha);
-		
-		txtDigiteSenha = new JPasswordField();
-		txtDigiteSenha.setFont(new Font("Calibri", Font.PLAIN, 18));
-		txtDigiteSenha.setToolTipText("Digite sua senha...");
-		txtDigiteSenha.setBounds(223, 399, 312, 31);
-		txtDigiteSenha.setBorder(new LineBorder(new Color(191, 63, 63), 2));
-		txtDigiteSenha.setBackground(new Color(227, 202, 187));
-		panelBranco.add(txtDigiteSenha);
-		
-		JButton buttonCriarConta = new JButton("Criar Conta");
-		buttonCriarConta.setBounds(500, 469, 121, 24);
-		buttonCriarConta.setForeground(new Color(227, 202, 187));
-		buttonCriarConta.setBackground(new Color(179, 13, 36));
-		buttonCriarConta.setFont(new Font("Calibri", Font.PLAIN, 15));
-		panelBranco.add(buttonCriarConta);
-		
-		txtConfirmeSenha = new JPasswordField();
-		txtConfirmeSenha.setToolTipText("Digite sua senha...");
-		txtConfirmeSenha.setFont(new Font("Calibri", Font.PLAIN, 18));
-		txtConfirmeSenha.setBorder(new LineBorder(new Color(191, 63, 63), 2));
-		txtConfirmeSenha.setBackground(new Color(227, 202, 187));
-		txtConfirmeSenha.setBounds(576, 399, 312, 31);
-		panelBranco.add(txtConfirmeSenha);
-
 	}
 }
