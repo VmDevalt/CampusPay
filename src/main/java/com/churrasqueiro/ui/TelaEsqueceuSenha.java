356c51256c46fc62ed6636cf98e63f810908de72
Matheus Ribeiro <matheus.jbr12345@gmail.com>
2025-11-28 01:08:13 -0300

    Configuração da janela e da paleta de cores da tela esqueceu a senha

--- src/main/java/com/churrasqueiro/ui/TelaEsqueceuSenha.java
+++ src/main/java/com/churrasqueiro/ui/TelaEsqueceuSenha.java
@@ -1,43 +1,50 @@
 package com.churrasqueiro.ui;
 
+import java.awt.Color;
 import java.awt.EventQueue;
-
 import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
 
 public class TelaEsqueceuSenha extends JFrame {
+    private static final long serialVersionUID = 1L;
+    private JPanel panelVermelho;
+    private static final int LARGURA = 1280;
+    private static final int ALTURA = 720;
+    
+    public static void main(String[] args) {
+        EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                try {
+                    TelaEsqueceuSenha frame = new TelaEsqueceuSenha();
+                    frame.setVisible(true);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+    }
+    
+    public TelaEsqueceuSenha() {
+    	
+        Color corPaletaVermelho = new Color(179,13,36);
+        Color corPaletaBege = new Color(227,202,187);
+        Color corPaletaVermelhoInteracao = new Color(200,50,50);
+        Color corPaletaVermelhoPressionado = new Color(150,0,0);
+        Color corPaletaPreto = new Color(0,0,0);
+        Color corPaletaCinza = new Color(140,127,127);
 
-	private JFrame frame;
-
-	/**
-	 * Launch the application.
-	 */
-	public static void main(String[] args) {
-		EventQueue.invokeLater(new Runnable() {
-			public void run() {
-				try {
-					TelaEsqueceuSenha window = new TelaEsqueceuSenha();
-					window.frame.setVisible(true);
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
-			}
-		});
-	}
-
-	/**
-	 * Create the application.
-	 */
-	public TelaEsqueceuSenha() {
-		initialize();
-	}
-
-	/**
-	 * Initialize the contents of the frame.
-	 */
-	private void initialize() {
-		frame = new JFrame();
-		frame.setBounds(100, 100, 1280, 720);
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-	}
-
+        setTitle("Esqueceu a Senha - Churrasqueiro");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setBounds(100, 100, LARGURA, ALTURA);
+        setSize(LARGURA, ALTURA);
+        setResizable(false);
+        setLocationRelativeTo(null);
+        
+        panelVermelho = new JPanel();
+        panelVermelho.setBackground(new Color(179, 13, 36));
+        panelVermelho.setBorder(new EmptyBorder(5, 5, 5, 5));
+        setContentPane(panelVermelho);
+        panelVermelho.setLayout(null);
+    }
 }
